b6e56fbe056012d4b510d5f4e67f517c7ebe90d6
ntutangyun <ntutangyun@gmail.com>
2017-10-14 03:18:49 +0800

    Masonry Gallery Added

--- ntusecondhand/static/js/main.js
+++ ntusecondhand/static/js/main.js
@@ -21,7 +21,7 @@ MyMainAPP.mobileNav = function(){
 			$('#mobile-nav').removeClass('open');	
 		}
 	}
-}
+};
 
 MyMainAPP.listenerMenu = function(){
 	$('#mobile-nav').on('click', function(e){
@@ -39,57 +39,12 @@ MyMainAPP.listenerMenu = function(){
 		$('#mobile-nav').removeClass('open');
 		$('#navigation-mobile').slideUp(350, 'easeOutExpo');
 	});
-}
+};
 
 
 /* ==================================================
    Slider Options
 ================================================== */
-
-MyMainAPP.slider = function(){
-	$.supersized({
-		// Functionality
-		slideshow               :   1,			// Slideshow on/off
-		autoplay				:	1,			// Slideshow starts playing automatically
-		start_slide             :   1,			// Start slide (0 is random)
-		stop_loop				:	0,			// Pauses slideshow on last slide
-		random					: 	0,			// Randomize slide order (Ignores start slide)
-		slide_interval          :   12000,		// Length between transitions
-		transition              :   1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-		transition_speed		:	300,		// Speed of transition
-		new_window				:	1,			// Image links open in new window/tab
-		pause_hover             :   0,			// Pause slideshow on hover
-		keyboard_nav            :   1,			// Keyboard navigation on/off
-		performance				:	1,			// 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
-		image_protect			:	1,			// Disables image dragging and right click with Javascript
-
-		// Size & Position						   
-		min_width		        :   0,			// Min width allowed (in pixels)
-		min_height		        :   0,			// Min height allowed (in pixels)
-		vertical_center         :   1,			// Vertically center background
-		horizontal_center       :   1,			// Horizontally center background
-		fit_always				:	0,			// Image will never exceed browser width or height (Ignores min. dimensions)
-		fit_portrait         	:   1,			// Portrait images will not exceed browser height
-		fit_landscape			:   0,			// Landscape images will not exceed browser width
-
-		// Components							
-		slide_links				:	'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
-		thumb_links				:	0,			// Individual thumb links for each slide
-		thumbnail_navigation    :   0,			// Thumbnail navigation
-		slides 					:  	[			// Slideshow Images
-											{image : "/static/img/image01.jpg", title : '<div class="slide-content">NTU Second Hand Exchange</div>', thumb : '', url : ''},
-											{image : "/static/img/image02.jpg", title : '<div class="slide-content">NTU Second Hand Exchange</div>', thumb : '', url : ''},
-											{image : "/static/img/image03.jpg", title : '<div class="slide-content">NTU Second Hand Exchange</div>', thumb : '', url : ''},
-											{image : "/static/img/image04.jpg", title : '<div class="slide-content">NTU Second Hand Exchange</div>', thumb : '', url : ''}
-									],
-
-		// Theme Options			   
-		progress_bar			:	0,			// Timer for each slide							
-		mouse_scrub				:	0
-		
-	});
-
-}
 
 
 /* ==================================================
@@ -98,7 +53,7 @@ MyMainAPP.slider = function(){
 
 MyMainAPP.nav = function(){
 	$('.sticky-nav').waypoint('sticky');
-}
+};
 
 
 /* ==================================================
@@ -151,7 +106,7 @@ MyMainAPP.filter = function (){
 			return false;
 		});
 	}
-}
+};
 
 
 /* ==================================================
@@ -180,7 +135,7 @@ MyMainAPP.fancyBox = function(){
 			}
 		});
 	}
-}
+};
 
 
 /* ==================================================
@@ -210,7 +165,7 @@ MyMainAPP.contactForm = function(){
 		});
 		return false;
 	});
-}
+};
 
 
 /* ==================================================
@@ -383,52 +338,8 @@ MyMainAPP.toolTip = function(){
 	Init
 ================================================== */
 
-MyMainAPP.slider();
-
 $(document).ready(function(){
-	Modernizr.load([
-	{
-		test: Modernizr.placeholder,
-		nope: "/static/js/placeholder.js",
-		complete : function() {
-				if (!Modernizr.placeholder) {
-						Placeholders.init({
-						live: true,
-						hideOnFocus: false,
-						className: "yourClass",
-						textColor: "#999"
-						});
-				}
-		}
-	}
-	]);
-	
-	// Preload the page with jPreLoader
-	$('body').jpreLoader({
-		splashID: "#jSplash",
-		showSplash: true,
-		showPercentage: true,
-		autoClose: true,
-		splashFunction: function() {
-			$('#circle').delay(250).animate({'opacity' : 1}, 500, 'linear');
-		}
-	});
-	
-	MyMainAPP.nav();
-	MyMainAPP.mobileNav();
-	MyMainAPP.listenerMenu();
-	MyMainAPP.menu();
-	MyMainAPP.goSection();
-	MyMainAPP.goUp();
-	MyMainAPP.filter();
-	MyMainAPP.fancyBox();
-	MyMainAPP.contactForm();
-	MyMainAPP.tweetFeed();
-	MyMainAPP.scrollToTop();
-	MyMainAPP.utils();
-	MyMainAPP.accordion();
-	MyMainAPP.toggle();
-	MyMainAPP.toolTip();
+
 });
 
 $(window).resize(function(){
