f7f23b7e89e5d8dbb5a971106badba20db6ac9f2
ntutangyun <ntutangyun@gmail.com>
2017-10-14 23:55:15 +0800

    main.js updated

--- ntusecondhand/static/js/main.js
+++ ntusecondhand/static/js/main.js
@@ -5,11 +5,11 @@ $(document).ready(function () {
     var modal_img = document.getElementById("id_modal_img");
     var modal_caption = document.getElementById("id_modal_caption");
 
-    $('.item_img_thumb').onclick = function () {
+    $(".item_img_thumb").click(function () {
         modal_container.style.display = "block";
         modal_img.src = this.src;
         modal_caption.innerHTML = this.alt;
-    };
+    });
 
     // Get the <modal_span> element that closes the modal_container
     var modal_span = document.getElementsByClassName("modal_close")[0];
@@ -17,5 +17,7 @@ $(document).ready(function () {
     // When the user clicks on <modal_span> (x), close the modal_container
     modal_span.onclick = function () {
         modal_container.style.display = "none";
-    }
+    };
+
+    alert('ready');
 });
