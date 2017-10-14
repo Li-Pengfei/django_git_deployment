8bb5b244d7977802835f9e34537c27023dbf6cd7
ntutangyun <ntutangyun@gmail.com>
2017-10-15 03:58:08 +0800

    modal item description added

--- ntusecondhand/static/js/main.js
+++ ntusecondhand/static/js/main.js
@@ -1,14 +1,9 @@
 $(document).ready(function () {
 
-    // Get the modal_container
-    var modal_container = document.getElementById('id_modal_container');
-    var modal_img = document.getElementById("id_modal_img");
-    var modal_caption = document.getElementById("id_modal_details");
-
-    $(".item_img_thumb").click(function () {
-        modal_container.style.display = "block";
-        modal_img.src = this.src;
-        modal_caption.innerHTML = this.alt;
+    $(".masonry-img-thumb").click(function () {
+        $('#id_modal_container').css('display', 'block');
+        // $('#id_modal_img').attr('src', this.src);
+        $('#id_modal_details').html(this.parentElement.innerHTML);
     });
 
     // Get the <modal_span> element that closes the modal_container
@@ -16,6 +11,6 @@ $(document).ready(function () {
 
     // When the user clicks on <modal_span> (x), close the modal_container
     modal_span.onclick = function () {
-        modal_container.style.display = "none";
+        $('#id_modal_container').css('display', 'none');
     };
 });
