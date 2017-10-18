00db7167070c524c4be0d293ffd7c205758a353c
ntutangyun <ntutangyun@gmail.com>
2017-10-18 14:13:26 +0800

    wrap up

--- ntusecondhand/mainApp/admin.py
+++ ntusecondhand/mainApp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from mainApp.models import UserProfileInfo, ItemModel
+from mainApp.models import UserProfileInfo, ItemModel, Offer
 
 
 # Register your models here.
 admin.site.register(UserProfileInfo)
 admin.site.register(ItemModel)
+admin.site.register(Offer)
